f1a47781b45ad9ec511dd137a3ec7abb4abc769c
Henrique Friggi <91426980+Hfriggi@users.noreply.github.com>
2022-04-05 01:33:29 -0300

    Modularization

--- ASTROBOT.py
+++ ASTROBOT.py
@@ -6,60 +6,54 @@ import urllib.request
 from PIL import Image
 import os
 
-keysTXT = open('keys.txt', 'r')
+keysTxtFile = open('keys.txt', 'r')
 
-api_key = keysTXT.readline().strip()
-api_secret_key = keysTXT.readline().strip()
-access_token = keysTXT.readline().strip()
-access_token_secret = keysTXT.readline().strip()
+apiKey = keysTxtFile.readline().strip()
+apiSecretKey = keysTxtFile.readline().strip()
+accessToken = keysTxtFile.readline().strip()
+accessSecretToken = keysTxtFile.readline().strip()
 
-auth = tweepy.OAuthHandler(api_key, api_secret_key)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit = True)
-
-#ASTROBIN
+authentificatorApi = tweepy.OAuthHandler(apiKey, apiSecretKey)
+authentificatorApi.set_access_token(accessToken, accessSecretToken)
+api = tweepy.API(authentificatorApi, wait_on_rate_limit = True)
 
 def getdata(url): 
     r = requests.get(url) 
     return r.text 
 
+def download_image(url):
+    fullname = "image_to_upload.jpeg"
+    urllib.request.urlretrieve(url,fullname) 
+
+#ASTROBIN
+
 htmldata = getdata("https://www.astrobin.com/iotd/archive/") 
 soup = BeautifulSoup(htmldata, 'html.parser')
 
-titulo = soup.findAll("h3")
-credito = soup.find('div', class_= 'data hidden-phone').find('p').find('a')
-endereco = soup.find('figure').find('a')
+tittle = soup.find('div', class_= 'iotd-archive-image').find('h3')
+credits = soup.find('div', class_= 'data hidden-phone').find('p').find('a')
+imageAddress = soup.find('figure').find('a').get('href')
 
-data = soup.find_all('div', class_="astrobin-image-container")
-a_class = data[0].find_all('a')
-url_ = a_class[0].get('href')
+linkImageUrl = 'https://www.astrobin.com/full' + imageAddress
+linkImageSpecs = 'https://www.astrobin.com' + imageAddress
 
-linke = 'https://www.astrobin.com/full' + url_
-link_ = 'https://www.astrobin.com' + url_
-
-g = requests.get(link_)
-soup = BeautifulSoup(g.text, 'html.parser')
+htmldata = getdata(linkImageSpecs)
+soup = BeautifulSoup(htmldata, 'html.parser')
 
 infos = soup.find( "div", class_= 'body').text.strip('\n')
-specs = infos.replace('Imaging Telescopes Or Lenses', 'Telescope:').replace('Imaging Cameras', 'Camera:').replace('Software','Software:').replace('Accessories','Accessories:').replace('Guiding Telescopes Or Lenses','GuideScope:').replace('Guiding Cameras','Guider:').replace('Mounts', 'Mount:').replace('Filters', 'Filters:')
+specs = specs.replace('Imaging Telescopes Or Lenses', 'Telescope:').replace('Imaging Cameras', 'Camera:').replace('Software','Software:').replace('Accessories','Accessories:')
+specs = specs.replace('Guiding Telescopes Or Lenses','GuideScope:').replace('Guiding Cameras','Guider:').replace('Mounts', 'Mount:').replace('Filters', 'Filters:')
 specs = specs.replace('\n\n·\n\n', ', ')
 specs = specs.encode('utf-8').decode('ascii', 'ignore')
+specs = specs[:280]
 
-if len(specs) > 279:
-    specs = specs[:279]
+htmldata = getdata(linkImageUrl)
+soup = BeautifulSoup(htmldata, 'html.parser')
 
-h = requests.get(linke)
-soup = BeautifulSoup(h.text, 'html.parser')
-
-images = soup.find('figure')
-
-for image in images:
-    name = image['alt']
-    link = image['src']   
-    
+astrobinImageLink = soup.find('figure').find('img')['src']
 
 path = "astrobin.jpg"
-response = requests.get(link, headers={'User-Agent': 'Mozilla/5.0'})
+response = requests.get(astrobinImageLink, headers={'User-Agent': 'Mozilla/5.0'})
 file = open(path, "wb")
 file.write(response.content)
 
@@ -72,15 +66,13 @@ if imageSize > 5000000:
     rgb_foo = rgb_foo.resize((imageSize), Image.ANTIALIAS)
     rgb_foo.save('astrobin.jpg', optimize=True, quality=85)
 
-texto_credito2 = 'Image Credit & Copyright: ' + credito.text.strip()
+creditsText = 'Image Credit & Copyright: ' + credits.text.strip()
 
-textoposta = 'Image Of The Day (IOTD) by Astrobin: \n' + titulo[0].text.strip() + '\n' + texto_credito2 + '\n' + linke + '\n' + '#astrophotography #astronomy #APOD #nasa #astrobin' + '\n'
-
-if len(textoposta) > 279:
-    textoposta = textoposta[:279]
+postText = 'Image Of The Day (IOTD) by Astrobin: \n' + tittle.text + '\n' + creditsText + '\n' + linkImageUrl + '\n' + '#astrophotography #astronomy #APOD #nasa #astrobin' + '\n'
+postText[:280]
 
 media_id = api.media_upload('astrobin.jpg').media_id
-original_tweet = api.update_status(textoposta, media_ids=[media_id])
+original_tweet = api.update_status(postText, media_ids=[media_id])
 reply1_tweet = api.update_status(status=specs, in_reply_to_status_id=original_tweet.id, auto_populate_reply_metadata = True)
 
 #NASA
@@ -89,29 +81,21 @@ def getdata2(url):
     r = requests.get(url) 
     return r.text 
 
-htmldata = getdata2("https://apod.nasa.gov/apod/astropix.html") 
+htmldata = getdata("https://apod.nasa.gov/apod/astropix.html") 
 soup = BeautifulSoup(htmldata, 'html.parser')
 
 infos = soup.findAll("center")[1]
 
-images = soup.find_all('img')
-for image in images:
-    name = image['alt']
-    link = image['src']
-        
-    realImage = 'https://apod.nasa.gov/apod/' + link
+linkToNasaImage = soup.find('img')['src']   
+nasaImageLink = 'https://apod.nasa.gov/apod/' + linkToNasaImage        
+download_image(nasaImageLink)
 
-def download_image(url):
-    fullname = "image_to_upload.jpeg"
-    urllib.request.urlretrieve(url,fullname)     
-download_image(realImage)
+infos = infos.text.strip().replace('Copyright:', '').replace('&', '').replace('Image Credit', 'Image Credits:') + '\n'
 
-texto_credito = infos.text.strip().replace('Copyright:', '').replace('&', '').replace('Image Credit', 'Image Credits:') + '\n'
+apodLink = 'https://apod.nasa.gov/apod/astropix.html \n'
 
-apod = 'https://apod.nasa.gov/apod/astropix.html \n'
+postText2 = 'Astronomy Picture of the Day (APOD) by apod.nasa:\n'  + infos + apodLink + '#astrophotography #astronomy #APOD #nasa #astrobin'
 
-titulo = 'Astronomy Picture of the Day (APOD) by apod.nasa:\n'  + texto_credito + apod + '#astrophotography #astronomy #APOD #nasa #astrobin'
+api.update_status_with_media(PostText2, 'image_to_upload.jpeg')
 
-api.update_status_with_media(titulo, 'image_to_upload.jpeg')
-
-keysTXT.close()
+keysTxtFile.close()
